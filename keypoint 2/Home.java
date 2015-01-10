f5ce93e788ee6534dcb4c98af1e6d5e8125cd053
marusack <marielsack@gmail.com>
2015-01-09 21:07:20 -0300

    keypoint 2-Design a building like a house and provide a builder to build it.

---
+++ keypoint 2/Home.java
@@ -0,0 +1,24 @@
+/**
+ * @author mariel
+ *
+ */
+public class Home extends Building {
+
+	@Override
+	public String buildRoom() {
+		
+		return ("one room was created for the family");
+	}
+
+	@Override
+	public String buildBathroom() {
+		
+		return ("one Bathroom was created for the family");
+	}
+
+	@Override
+	public String buildKitchen() {
+		
+		return ("one Kitchen was created for the family");
+	}
+}
