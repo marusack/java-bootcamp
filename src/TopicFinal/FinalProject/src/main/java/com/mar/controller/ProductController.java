36a416f7957b44a43ff61a9f6664d147e6afb41b
marusack <marielsack@gmail.com>
2015-02-11 02:46:50 -0300

    servlet problem .. yet unfinished

---
+++ src/TopicFinal/FinalProject/src/main/java/com/mar/controller/ProductController.java
@@ -0,0 +1,57 @@
+package com.mar.controller;
+
+import java.util.Map;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mar.model.Product;
+import com.mar.service.ProductService;
+@RestController
+public class ProductController {
+
+
+	
+	private ProductService service;
+	
+	@RequestMapping("/indexproduct")
+	public String setupForm(Map<String,Object>map){
+		Product prod = new Product();
+		map.put("product", prod);
+		map.put("productList", service.findAll());
+		return"product";
+	}
+	
+	@RequestMapping(value="/product.do", method=RequestMethod.POST)
+	public String doActions(@ModelAttribute Product prod, BindingResult resut, 
+			@RequestParam String action, Map<String,Object>map){
+		
+		Product Result = new Product();
+		switch(action.toLowerCase()){
+		case "add":
+			service.addProduct(prod);
+			Result=prod;
+			break;
+		case "edit":
+			service.editProduct(prod);
+			Result=prod;
+			break;
+		case "delete":
+			service.deleteProduct(prod.getId());
+			Result=prod;
+			break;
+		case"search":
+			Product searchedprod =service.findById(prod.getId());
+			Result= searchedprod!=null ? searchedprod : new Product();
+			break;
+			
+		}
+		map.put("product", Result);
+		map.put("productList", service.findAll());
+		return "product";
+	}
+}
---
+++ src/TopicFinal/FinalProject/src/main/java/com/mar/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.mar.controller;
+
+import java.util.Map;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mar.model.User;
+import com.mar.service.UserService;
+
+@RestController
+public class UserController {
+		
+
+	
+	private UserService service;
+	
+	@RequestMapping("/indexuser")
+	public String setupForm(Map<String,Object>map){
+		User user = new User();
+		map.put("user", user);
+		map.put("userList", service.findAll());
+		return"user";
+	}
+	
+	@RequestMapping(value="/user.do", method=RequestMethod.POST)
+	public String doActions(@ModelAttribute User user, BindingResult resut, 
+			@RequestParam String action, Map<String,Object>map){
+		
+		User Result = new User();
+		switch(action.toLowerCase()){
+		case "add":
+			service.addUser(user);
+			Result=user;
+			break;
+		case "edit":
+			service.editUser(user);
+			Result=user;
+			break;
+		case "delete":
+			service.deleteUser(user.getId());
+			Result=user;
+			break;
+		case"search":
+			User searcheduser =service.findById(user.getId());
+			Result= searcheduser!=null ? searcheduser : new User();
+			break;
+			
+		}
+		map.put("user", Result);
+		map.put("userList", service.findAll());
+		return "user";
+	}
+}
